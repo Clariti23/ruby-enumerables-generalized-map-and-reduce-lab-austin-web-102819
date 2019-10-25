6d4d2feb81e365b6d625ef592cbfe25a995e23bb
Grant Ferowich <g.ferowich@gmail.com>
2019-10-25 21:53:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,48 +1,25 @@
-def map_to_negativize(source_array)
-source_array.map {|n| n* -1 }
-end
-
-def map_to_no_change(source_array)
-  source_array.map {|n| n}
-end
- 
-
-def map_to_double(source_array)
-  source_array.map {|i| i * 2}
-end
-
-def map_to_square(source_array)
- source_array.map {|j| j * j}
-end
-
-def reduce_to_total(source_array, starting_point=0)
-  total = starting_point
-  
-  i = 0 
-  while i < source_array.length 
-    total += source_array[i]
+def map(s)
+  new = []
+  i = 0
+  while i < s.length
+    new.push(yield(s[i]))
     i += 1
   end
+  new
+end
 
-  total  
-end  
-
-def reduce_to_all_true(source_array)
-  i = 0 
-  while i < source_array.length
-    return false if !source_array[i]
+def reduce(s, sp=nil)
+  if sp
+    accum = sp
+    i = 0
+  else
+    accum = s[0]
+    i = 1
+  end
+  while i < s.length
+    accum = yield(accum, s[i])
     i += 1
   end
-  return true 
+  accum
 end
-#reviews^^
-def reduce_to_any_true(source_array)
-  i = 0 
-  while i < source_array.length
-    return true if source_array[i]
-    i += 1
-  end
-  return false
-end
-#reviews^^
 
